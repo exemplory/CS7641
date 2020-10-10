86b792926796f6a57794657c626694e631494e98
exemplory <vivek.agrawal.01@gmail.com>
2020-10-10 16:10:09 -0700

    Update Experiments.java

--- A2-RandomizedOptimization/src/vagrawal63/a2/opt/Experiments.java
+++ A2-RandomizedOptimization/src/vagrawal63/a2/opt/Experiments.java
@@ -8,16 +8,16 @@ public class Experiments {
 	
 	public static void main(String[] args) {
 		ContinuousPeak cp = new ContinuousPeak();
-		//cp.fit(5);
-		//cp.tune(5);
+		cp.fit(5);
+		cp.tune(5);
 		
 		FourPeaks fp = new FourPeaks();
 		fp.fit(5);
-		//fp.tune(5);
+		fp.tune(5);
 
 		TravellingSalesman ts = new TravellingSalesman();
-		//ts.fit(5);
-		//ts.tune(5);
+		ts.fit(5);
+		ts.tune(5);
 		
 	}
 }
